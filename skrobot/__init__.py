8e59a1c011ff048c2c73e20bf7e630b592530e2e
Shuhei <takamatushuhei@outlook.jp>
2023-08-04 17:01:15 +0900

    remove sdf lib

--- skrobot/__init__.py
+++ skrobot/__init__.py
@@ -2,19 +2,17 @@
 
 import pkg_resources
 
-
 __version__ = pkg_resources.get_distribution('scikit-robot').version
-
 
 from skrobot import coordinates
 from skrobot import data
 from skrobot import interpolator
 from skrobot import optimizer
 from skrobot import optimizers
-from skrobot import planner
+# from skrobot import planner
 from skrobot import interfaces
-from skrobot import model
-from skrobot import models
-from skrobot import viewers
+# from skrobot import model
+# from skrobot import models
+# from skrobot import viewers
 from skrobot import utils
-from skrobot import sdf
+# from skrobot import sdf
